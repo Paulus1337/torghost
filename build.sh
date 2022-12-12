088924b00cf2dcbed4d87d8fd99367a63911d6d7
Paulus88 <paulhemmens@live.nl>
2022-12-13 00:18:13 +0100

    Update build.sh
    Updated build.sh to work with python3.11
    Enjoy

--- build.sh
+++ build.sh
@@ -1,29 +1,28 @@
-echo "Torghost installer v3.0"
+echo "Torghost installer v3.0.1"
 echo "Installing prerequisites "
-sudo apt-get install tor python3-pip -y 
+sudo apt-get install tor python3-pip python3.11 python3.11-dev -y
 echo "Installing dependencies "
 sudo pip3 install -r requirements.txt 
 mkdir build
 cd build
-cython3 ../torghost.py --embed -o torghost.c --verbose
+cython3 ../torghost.py --embed -o torghost.c --verbose -X language_level=3
 if [ $? -eq 0 ]; then
     echo [SUCCESS] Generated C code
 else
     echo [ERROR] Build failed. Unable to generate C code using cython3
     exit 1
 fi
-gcc -Os -I /usr/include/python3.8 -o torghost torghost.c -lpython3.8 -lpthread -lm -lutil -ldl
+gcc -Os -I /usr/include/python3.11 -o torghost torghost.c -lpython3.11 -lpthread -lm -lutil -ldl
 if [ $? -eq 0 ]; then
     echo [SUCCESS] Compiled to static binay 
 else
     echo [ERROR] Build failed
     exit 1
 fi
-sudo cp -r torghost /usr/bin/
+sudo mv torghost /usr/bin/torghost
 if [ $? -eq 0 ]; then
-    echo [SUCCESS] Copied binary to /usr/bin 
+    echo [SUCCESS] Moved binary to /usr/bin 
 else
     echo [ERROR] Unable to copy
-    ecit 1
+    exit 1
 fi
-
